ff6b8caef8a28ed736d74270f21598b1187335ed
Chris Liechti <cliechti@gmx.net>
2017-05-11 22:12:59 +0200

    feat: add __main__

---
+++ aafigure/__main__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#
+# This file is part of aafigure. https://github.com/aafigure/aafigure
+# (C) 2017 Chris Liechti <cliechti@gmx.net>
+#
+# SPDX-License-Identifier:    BSD-3-Clause
+
+# command line stub for aafigure
+
+from . import aafigure
+aafigure.main()
